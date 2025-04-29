28404007c7b468ba92e131884748fb4bc44d406e
tbartsctr <tbartsctr@gmail.com>
2025-04-29 12:28:24 -0400

    BecomePatientHeaderAdd

--- src/BecomePatient.jsx
+++ src/BecomePatient.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "./Layout";
 import BannerImage from "./NewPatientsImg1.jpg";
 import BodyImage from "./NewPatientsImage1.webp";
+import "./BecomePatient.css";
 
 
 function BecomePatientHeader () {
